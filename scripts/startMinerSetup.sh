c87839a7c509c97765314ba43f9cd73f4a0f7154
michael-cyber-pro <michael_wu@nimble.technology>
2024-10-16 23:16:12 +0800

    download minersetup every time

--- scripts/startMinerSetup.sh
+++ scripts/startMinerSetup.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+wget -O minerSetup.sh https://raw.githubusercontent.com/nimble-technology/nimble-miner-public/main/scripts/minerSetup.sh
 chmod +x minerSetup.sh
 pkill -f ./minerSetup.sh
 nohup ./minerSetup.sh ubuntu ~/.ssh/id_rsa.pub  notebook.nimble.technology &
